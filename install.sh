5bf7242d0d22617dedebe12b9a2a436e57c2815a
Egor <megametagross@outlook.de>
2023-02-18 21:15:57 +0300

    added bash init

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh &
     bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda && \
     rm Miniconda3-latest-Linux-x86_64.sh
 
+conda init bash
 conda env create -f conda-env.yaml python=3.10.9
 conda activate aivtuber
 
