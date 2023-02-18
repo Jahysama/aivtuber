8698cfd72442b152434f8fb682c5b03e8fedd5fc
Egor <megametagross@outlook.de>
2023-02-18 21:24:09 +0300

    changed conda activation

--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@ export PATH=/opt/conda/bin:${PATH}
 wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
     bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda && \
     rm Miniconda3-latest-Linux-x86_64.sh
+echo "source activate aivtuber" > ~/.bashrc
+export PATH=/opt/conda/envs/aivtuber/bin:${PATH}
 
-conda init bash
 conda env create -f conda-env.yaml python=3.10.9
 conda activate aivtuber
 
-cd utils
+cd src/utils
 
 git clone https://github.com/jnordberg/tortoise-tts.git
 cd tortoise-tts
